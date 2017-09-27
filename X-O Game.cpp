689bf964d2ed48a3fb8e74d13c1ea20c22d3ad63
mustafa-jalal <32310813+mustafa-jalal@users.noreply.github.com>
2017-09-27 13:56:09 -0700

    Add files via upload

---
+++ X-O Game.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <windows.h>
+#include "../X-O Game/headers/play.h"
+#include "../X-O Game/headers/winner.h"
+using namespace std;
+
+int main (){
+    while (winner() == 's' && counter < 9){
+        matrix_pr();
+        play();
+    }
+
+    if (winner() == 'x'){
+        cout<<"congrats <<X>> the winner";
+
+    }else if (winner() == 'o'){
+    cout<<"congrats <<O>> the winner";
+
+    }
+    if (counter >= 9){
+        cout<<"Game is finished without winner";
+    }
+
+            }
